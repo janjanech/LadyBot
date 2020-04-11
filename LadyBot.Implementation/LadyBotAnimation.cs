4ba74afc0fe7b30fc9874bc590442da0f1c80eef
Jan Janech <jan.janech@kst.uniza.sk>
2020-04-12 00:17:15 +0200

    ladybot moves using SMIL animations

---
+++ LadyBot.Implementation/LadyBotAnimation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace LadyBot.Implementation
+{
+	internal abstract class LadyBotAnimation
+	{
+		public class Rotation : LadyBotAnimation
+		{
+			public Rotation(int oldRotation, int newRotation)
+			{
+				if ((newRotation - oldRotation + 360) % 360 > 180)
+					newRotation -= 360;
+				this.OldRotation = oldRotation;
+				this.NewRotation = newRotation;
+			}
+
+			public int OldRotation { get; }
+
+			public int NewRotation { get; }
+		}
+
+
+		public class Translation : LadyBotAnimation
+		{
+			public Translation(int newX, int newY)
+			{
+				this.NewX = newX;
+				this.NewY = newY;
+			}
+
+			public int NewX { get; }
+			public int NewY { get; }
+		}
+	}
+}
