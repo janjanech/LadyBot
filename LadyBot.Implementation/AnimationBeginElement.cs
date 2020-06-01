6367ac75b95c7804e6ddf68135dda0a3e3172f25
Jan Janech <jan.janech@kst.uniza.sk>
2020-06-01 12:31:07 +0200

    AnimationBeginElement refactored to c# class from razor

---
+++ LadyBot.Implementation/AnimationBeginElement.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace LadyBot.Implementation
+{
+	public class AnimationBeginElement : ComponentBase
+	{
+		private bool aFirstTime = true;
+
+		[Parameter]
+		public string Id { get; set; }
+
+		[Inject]
+		private IJSRuntime Js { get; set; }
+
+		protected override async Task OnAfterRenderAsync(bool firstRender)
+		{
+			await base.OnAfterRenderAsync(firstRender);
+			if (this.aFirstTime)
+				await this.Js.InvokeVoidAsync("beginAnimations", (object) this.Id);
+			this.aFirstTime = false;
+		}
+	}
+}
